af3c38321945e01d8bbab2f7960ed44b6449367c
Victor Vidales <victor_vidales97@hotmail.com>
2019-12-09 22:17:11 -0600

    Personalización de colores

--- src/routes/u-next.ts
+++ src/routes/u-next.ts
@@ -14,32 +14,37 @@ app.set('views', path.resolve(__dirname, '../views'));
 
 handlebars.registerPartials(path.resolve(__dirname, '../views/partials'));
 
-app.get('/', [validarSesion], (req: Request, res: Response) => {
-    res.redirect('/index');
+app.get('/', [validarSesion], async (req: Request, res: Response) => {
+    const empresa = await Cuenta.findOne({ _id: req.session!.empresa });
+    const img = base64('favicon2.png');
+    res.render('index', {img, active: {Inicio: true }, color1: empresa!.color1, color2: empresa!.color2});
 });
 
 app.get('/configuracion', [validarSesion], async (req: Request, res: Response) => {
     const empresa = await Cuenta.findOne({ _id: req.session!.empresa });
     const imgConfiguracion = base64(empresa!.imagenEmpresa);
     const img = base64('favicon2.png');
-    res.render('configuracion', {imgConfiguracion, img, active: {Configuracion: true }});
+    res.render('configuracion', {imgConfiguracion, img, active: {Configuracion: true }, color1: empresa!.color1, color2: empresa!.color2});
 });
 
 app.get('/escritorios', [validarSesion], async (req: Request, res: Response) => {
+    const empresa = await Cuenta.findOne({ _id: req.session!.empresa });
     const escritorios = await Escritorio.find({ idCuenta: req.session!.empresa, activo: true });
     const img = base64('favicon2.png');
-    res.render('escritorios', {img, active: {Escritorios: true }, escritorios});
+    res.render('escritorios', {img, active: {Escritorios: true }, escritorios, color1: empresa!.color1, color2: empresa!.color2});
 });
 
 
-app.get('/escritorio', [validarSesion], (req: Request, res: Response) => {
+app.get('/escritorio', [validarSesion], async (req: Request, res: Response) => {
+    const empresa = await Cuenta.findOne({ _id: req.session!.empresa });
     const img = base64('favicon2.png');
-    res.render('escritorio', {img});
+    res.render('escritorio', {img, color1: empresa!.color1, color2: empresa!.color2});
 });
 
-app.get('/index', [validarSesion], (req: Request, res: Response) => {
+app.get('/index', [validarSesion], async (req: Request, res: Response) => {
+    const empresa = await Cuenta.findOne({ _id: req.session!.empresa });
     const img = base64('favicon2.png');
-    res.render('index', {img, active: {Inicio: true }});
+    res.render('index', {img, active: {Inicio: true }, color1: empresa!.color1, color2: empresa!.color2});
 });
 
 app.get('/inicio-de-sesion', (req: Request, res: Response) => {
@@ -68,18 +73,21 @@ app.post('/inicio-de-sesion', (req: Request, res: Response) => {
     });
 });
 
-app.get('/nuevo-ticket', [validarSesion], (req: Request, res: Response) => {
+app.get('/nuevo-ticket', [validarSesion], async (req: Request, res: Response) => {
+    const empresa = await Cuenta.findOne({ _id: req.session!.empresa });
     const img = base64('favicon2.png');
-    res.render('nuevo-ticket', {img});
+    res.render('nuevo-ticket', {img, color1: empresa!.color1, color2: empresa!.color2});
 });
 
-app.get('/publico', [validarSesion], (req: Request, res: Response) => {
-    res.render('publico');
+app.get('/publico', [validarSesion], async (req: Request, res: Response) => {
+    const empresa = await Cuenta.findOne({ _id: req.session!.empresa });
+    res.render('publico', {color1: empresa!.color1, color2: empresa!.color2});
 });
 
-app.get('/detalles-ticket', (req: Request, res: Response) => {
+app.get('/detalles-ticket', async (req: Request, res: Response) => {
+    const empresa = await Cuenta.findOne({ _id: req.session!.empresa });
     const img = base64('favicon2.png');
-    res.render('detalles-ticket', {img});
+    res.render('detalles-ticket', {img, color1: empresa!.color1, color2: empresa!.color2});
 });
 
 export = app;
