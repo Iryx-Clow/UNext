4e69f86b7f7e898592b52f5d5bea0993d9e03d5c
Daniel Romo <danielromo8@hotmail.com>
2019-12-10 00:49:17 -0600

    generacion de codigo

--- src/public/js/socket.nuevo-ticket.js
+++ src/public/js/socket.nuevo-ticket.js
@@ -1,6 +1,6 @@
 // Comando para establecer la conexión
 var socket = io();
-
+var URLturno = "https://174.128.2.64"
 var label = $('#lblNuevoTicket');
 
 
@@ -28,5 +28,24 @@ $('button').on('click', function () {
         label.text(siguienteTicket);
 
     });
+
+    console.log("impresion ticket");
+    let impresora = new Impresora();
+    impresora.feed(1);
+    impresora.setAlign("center");
+    impresora.setFont("B");
+    impresora.setFontSize(2,2);
+    impresora.write("UNEXT");
+    impresora.feed(1);
+    var qrcode=URLturno+"/detalles-ticket?"+label.text();
+    impresora.qr(qrcode);
+    impresora.setEmphasize(1);
+    impresora.write(""+label.text());
+    impresora.feed(1);
+    impresora.setFont("A");
+    impresora.setFontSize(1,1);
+    //impresora.cut();
+    //impresora.end();
+    impresora.imprimirEnImpresora("XP-80C");
 
 });
